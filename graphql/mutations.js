c46f43f4156b7cf04825bd827e9e328c7052a57c
sergio <seanviar@hotmail.com>
2021-11-28 19:53:47 -0500

    editar informacion del usuario

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,25 +1,43 @@
 'use strict'
 
 const connectDB = require('../db/db')
+const { ObjectId } = require('mongodb')
 
 
 module.exports = {
-    createUsuario: async (root, {input}) => {
-       const defaults = {
-        estado:"pendiente"
-       }
-    const newUser = Object.assign(defaults, input)
-    let db
-    let usuario
-    try {
-        db = await connectDB()
-        usuario = await db.collection('usuarios').insertOne(newUser)
-         
-        
-    } catch (error) {
-        console.error(error)
+    createUsuario: async (root, { input }) => {
+        const defaults = {
+            estado: "pendiente"
+        }
+        const newUser = Object.assign(defaults, input)
+        let db
+        let usuario
+        try {
+            db = await connectDB()
+            usuario = await db.collection('usuarios').insertOne(newUser)
+
+
+        } catch (error) {
+            console.error(error)
+        }
+        return newUser
+    },
+    editUser: async (root, { _id, input }) => {
+        let db
+        let usuario
+        try {
+            db = await connectDB()
+            await db.collection('usuarios').updateOne(
+                { _id: ObjectId(_id) },
+                { $set: input }
+            )
+            usuario = await db.collection('usuarios').findOne(
+                { _id: ObjectId(_id) }
+            )
+        } catch (error) {
+            console.error(error)
+        }
+        return usuario
     }
-    return newUser
-}
 
 }
